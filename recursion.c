2e26bfe2b0d8f3dcc3b0be26230e9304c21810b4
Egor <78965361+Kloun456@users.noreply.github.com>
2021-03-22 21:18:54 +0300

    Update recursion.c

--- recursion.c
+++ recursion.c
@@ -4,14 +4,16 @@
 #include <ctype.h>
 const int n = 40;
 int count = 0;  // for symbols
+float eps = 1e-12;
 
-void Print_array(int *x), Fill_array(int *x);
+void Print_array(int *x), Fill_array(int *x), Print_numb(int *x, int );
 int min(int *x), min1(int *x, int , int), digits(FILE *my_file), symbols(FILE *my_file), sum(int *x, int );
+float Solution(float , float), f(float );
 
 
 int main()
 {
-    int x[n], i = 0;
+    int x[10] = {1,-2,-3,4,5,6,-7,8,9,0}, i = 0;
     char c;
     FILE *my_file;
 
@@ -21,14 +23,14 @@ int main()
     printf("\nmin = %d", min(x));*/
 
     // 2)
-    while(i < n)
+    /*while(i < n)
     {
         scanf("%d",&x[i]);
         if (x[i] < 0)
             break;
         i++;
     }
-    printf("Sum of elements = %d",sum(x,0));
+    printf("Sum of elements = %d",sum(x,0)); */
 
     // 3)
     /*if ((my_file = fopen("My_file.txt","r"))==0)
@@ -45,6 +47,45 @@ int main()
     printf("quantity digits = %d\n", digits(my_file));
     fclose(my_file); */
 
+    // 4)
+    /*Print_numb(x,i);*/
+
+    // 5)
+    printf("%f", Solution(0.94721,0.94722));
+}
+
+float Solution(float a, float b)
+{
+    float x = (a + b) / 2.0;
+    if (abs(a - b) < eps)
+        return x;
+    if (!f(x))
+        return x;
+    if (f(x) * f(a) > 0)
+        return Solution(x, b);
+    else
+        return Solution(a, x);
+}
+
+float f(float x)
+{
+    return sin(1+pow(x,2))-x;
+}
+
+void Print_numb(int *x, int i)
+{
+    if (x[i] < 0)
+    {
+        printf("%d\t",x[i]);
+        Print_numb(x,i+1);
+    }
+    else if (x[i] > 0)
+    {
+        Print_numb(x,i+1);
+        printf("%d\t",x[i]);
+    }
+    else
+        return 0;
 }
 
 int sum(int *x, int l)
