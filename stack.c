50680d3d0166063f97b45d9647b561f863960beb
Egor <78965361+Kloun456@users.noreply.github.com>
2021-04-11 17:20:58 +0300

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,69 +1,71 @@
 #include "stack.h"
 
-pStack Create_stack()
+
+STACK *Create_stack()
 {
-    pStack ps;
-    ps = (pStack)malloc(sizeof(pStack));
+    STACK *ps;
+    ps = (STACK *)malloc(sizeof(STACK));
     ps->first = 0;
     ps->len = 0;
     return ps;
 }
 
-void Push(pStack ps,char ch)
+void Push_front_stack(STACK *ps, int ch)
 {
-    pNode temp = ps->first;
+    NODE *temp = ps->first;
     if (ps->first == 0)
     {
-        ps->first = (pNode)malloc(sizeof(pNode));
+        ps->first = (NODE *)malloc(sizeof(NODE));
         ps->first->c = ch;
         ps->first->next = 0;
     }
     else
     {
-        ps->first = (pNode)malloc(sizeof(pNode));
+        ps->first = (NODE *)malloc(sizeof(NODE));
         ps->first->c = ch;
         ps->first->next = temp;
     }
     ps->len++;
 }
 
-char Pop(pStack ps)
+int Pop_front_stack(STACK *ps)
 {
-    char ch = ps->first->c;
-    pNode temp = ps->first;
+    int x = ps->first->c;
+    NODE *temp = ps->first;
     ps->first = temp->next;
     ps->len--;
     free(temp);
-    return ch;
+    temp = 0;
+    return x;
 }
 
-int Is_empty(pStack ps)
+int Is_empty_stack(STACK *ps)
 {
     if (ps->len && ps->first)
         return 0;
     return 1;
 }
 
-void Show_stack(pStack ps)
+void Show_stack(STACK *ps)
 {
-    pNode temp = ps->first;
-    if (Is_empty(ps))
+    NODE *temp = ps->first;
+    if (Is_empty_stack(ps))
         printf("\nStack is empty!\n");
     else
     {
-        while(temp)
+        while (temp)
         {
-            printf("%c\t",temp->c);
+            printf("%d\t", temp->c);
             temp = temp->next;
         }
     }
+    free(temp);
+    temp = 0;
 }
 
-void Clear_stack(pStack ps)
+void Clear_stack(STACK *ps)
 {
-    if (Is_empty(ps))
-        printf("\nStack is empty!");
-    else
-        while (!Is_empty(ps))
-            Pop(ps);
+    while (!Is_empty_stack(ps))
+        Pop_front_stack(ps);
 }
+
