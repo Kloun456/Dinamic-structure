72c5149c11048118b8b227287d8942e8773d67b3
Egor <78965361+Kloun456@users.noreply.github.com>
2021-04-12 16:46:15 +0300

    Add files via upload

---
+++ queue.c
@@ -0,0 +1,74 @@
+#include "queue.h"
+
+QUEUE *Create_queue()
+{
+    QUEUE *pq;
+    pq = (QUEUE *)malloc(sizeof(QUEUE));
+    pq->first = 0;
+    pq->len = 0;
+    return pq;
+}
+
+void Push_back_queue(QUEUE *pq,char ch)
+{
+    NODE *temp = pq->first;
+    if (pq->first == 0)
+    {
+        pq->first = (NODE *)malloc(sizeof(NODE));
+        pq->first->c = ch;
+        pq->first->next = 0;
+    }
+    else
+    {
+        while(temp->next != 0)
+        {
+            temp = temp->next;
+        }
+        temp->next = (NODE *)malloc(sizeof(NODE));
+        temp->next->c = ch;
+        temp->next->next = 0;
+    }
+    pq->len++;
+}
+
+char Pop_front_queue(QUEUE *pq)
+{
+    char ch;
+    NODE *temp = pq->first;
+    ch = pq->first->c;
+    pq->first = pq->first->next;
+    free(temp);
+    temp = 0;
+    pq->len--;
+
+    return ch;
+}
+
+int Is_empty_queue(QUEUE *pq)
+{
+    if (pq->len && pq->first)
+        return 0;
+    return 1;
+}
+
+void Show_queue(QUEUE *pq)
+{
+    NODE *temp = pq->first;
+    if (Is_empty_queue(pq))
+        printf("\nQueue is empty!\n");
+    else
+    {
+        while(temp)
+        {
+            printf("%c\t",temp->c);
+            temp = temp->next;
+        }
+    }
+}
+
+void Clear_queue(QUEUE *pq)
+{
+    while (!Is_empty_queue(pq))
+        Pop_front_queue(pq);
+}
+
