89c1939ebcbfee68ba28d2f7205c6a37e5300273
Egor <78965361+Kloun456@users.noreply.github.com>
2021-04-25 14:37:05 +0300

    Update queue.h

--- queue.h
+++ queue.h
@@ -15,13 +15,14 @@ typedef struct Queue
 
 }QUEUE;
 
-// Pop_back - на языке си реализовать нельзя
-
 QUEUE *Create_queue();
-void Push_back_queue(QUEUE *pq, char ch);
-char Pop_front_queue(QUEUE *pq);
+void Push_back_queue(QUEUE *pq, int ch);
+int Pop_front_queue(QUEUE *pq);
 int Is_empty_queue(QUEUE *pq);
 void Show_queue(QUEUE *pq);
 void Clear_queue(QUEUE *pq);
+void Fill_queue(QUEUE *pq);
+void Greatest_element(QUEUE *pq);
+void Section(QUEUE *pq);
 
 #endif // QUEUE_H_INCLUDED
