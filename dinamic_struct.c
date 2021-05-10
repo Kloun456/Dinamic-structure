70b8c765ef604966c698533535d175f3fb3e898e
Egor <78965361+Kloun456@users.noreply.github.com>
2021-05-10 13:45:02 +0300

    Update dinamic_struct.c

--- dinamic_struct.c
+++ dinamic_struct.c
@@ -1,104 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <math.h>
-#include "stack.h"
-#define N 20
-
-
-void Fill(STACK *st)
-{
-    srand(time(NULL));
-    for (int i = 0; i < N; i++)
-        Push_front_stack(st,rand() % 71 - 50);  // ãåíåðàòîð ÷èñåë îò -50 äî 50
-}
-
-void Sort(STACK *st, STACK *neg, STACK *pos)  // сортирует на два стека, один который содержит отрицательные числа, другой положительные
-{
-    while(!Is_empty_stack(st))
-    {
-        if (st->first->c < 0)
-            Push_front_stack(neg, st->first->c);
-        else
-            Push_front_stack(pos, st->first->c);
-        Pop_front_stack(st);
-    }
-}
-
-void Delete(STACK *st)  // удаляет четные числа
-{
-    STACK *del = Create_stack();
-    while(st->len)
-    {
-        if(st->first->c % 2 != 0)
-            Push_front_stack(del,Pop_front_stack(st));
-        else
-            Pop_front_stack(st);
-    }
-    while(del->len)
-        Push_front_stack(st,Pop_front_stack(del));
-
-}
-
-STACK *Swap(STACK *st)
-{
-    STACK *del = Create_stack();
-    int t = Pop_front_stack(st);
-    Push_front_stack(del,t);
-    for (int i = st->len; i > 0; i--)
-        Push_front_stack(del,Pop_front_stack(st));
-    return del;
-}
-
-void Delete_index(STACK *st) // удаляет каждый второй элемент
-{
-    STACK *del = Create_stack();
-    for(int i = st->len; i > 0; i--)
-    {
-        if(i % 2 == 0)
-            Push_front_stack(del,Pop_front_stack(st));
-        else
-            Pop_front_stack(st);
-    }
-    while(del->len)
-        Push_front_stack(st,Pop_front_stack(del));
-}
+#include "Binary_tree.h"
 
 int main()
 {
-    STACK *st, *neg, *pos;
-    st = Create_stack();
-    neg = Create_stack();
-    pos = Create_stack();
-    Fill(st);
+    NODE *p, *root = 0;
+    root = Add_node(8,root);
+    root = Add_node(3,root);
+    root = Add_node(6,root);
+    root = Add_node(10,root);
+    root = Add_node(1,root);
+    root = Add_node(4,root);
+    root = Add_node(7,root);
+    Del_tree(root,6);
+    printf("%d",root->left->right->key);
 
-    // 1
-    /*printf("\nMain stack:\n");
-    Show_stack(st);
-    Sort(st, neg, pos);
-    printf("\nNegative stack:\n");
-    Show_stack(neg);
-    printf("\nPositive stack:\n");
-    Show_stack(pos);*/
+
+    /*QUEUE *pq = Create_queue();
+
+    //1
+    Fill_queue(pq);
+    printf("Main queue:\n");
+    Show_queue(pq);
+    printf("\nQueue after operations:\n");
+    Greatest_element(pq);
+    Show_queue(pq);
 
     //2
-    /*printf("\nMain stack:\n");
-    Show_stack(st);
-    Delete(st);
-    printf("\nMain stack after delete:\n");
-    Show_stack(st);*/
-
-    //3
-    /*printf("\nMain stack:\n");
-    Show_stack(st);
-    neg = Swap(st);
-    printf("\nMain stack after swap:\n");
-    Show_stack(neg);*/
-
-    //4
-    /*printf("\nMain stack:\n");
-    Show_stack(st);
-    Delete_index(st);
-    printf("\nMain stack after delete:\n");
-    Show_stack(st);*/
+    Section(pq);
+    printf("\nQueue after operations:\n");
+    Show_queue(pq);*/
 }
